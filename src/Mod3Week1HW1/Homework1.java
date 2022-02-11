e6440c9f1b6b45635e84993d48e78c1a16759b9a
Carmen Guevara <carmen.guevara75@gmail.com>
2022-02-10 19:06:20 -0500

    Cleaning Up code1

--- src/Mod3Week1HW1/Homework1.java
+++ src/Mod3Week1HW1/Homework1.java
@@ -3,6 +3,9 @@ package Mod3Week1HW1;
 public class Homework1 {
 
 	public static boolean getUp(boolean squaking, int currentHour) {
+		
+		//squaking is a boolean and represent the parrot if false the parrot is not squaking;
+		//currentHour is an integer that represents the hours of the day
 
 		if (squaking) {
 			if (currentHour < 0 || currentHour > 23) {
@@ -16,9 +19,7 @@ public class Homework1 {
 		}
 	}
 
-	public static void main(String[] args) {
-		
-			System.out.println("Get up? "+ getUp(false,2));
+	public static void main(String[] args) {		
+			System.out.println("Get up? "+ getUp(true,5));
 	}
-
 }
